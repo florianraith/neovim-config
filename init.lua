88b44b5e299cfa0858702a22e142d15a37c576a9
Florian Raith <florianraith00@gmail.com>
2023-10-28 21:49:57 +0200

    Add lsp

--- init.lua
+++ init.lua
@@ -21,14 +21,14 @@ vim.keymap.set('n', '<Leader><Space>', ':nohlsearch<CR>')
 
 
 -- bootstrap lazy.nvim plugin manager
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
     lazypath,
   })
 end
@@ -36,10 +36,16 @@ vim.opt.rtp:prepend(lazypath)
 
 
 -- setup plugins
-require("lazy").setup({
+require('lazy').setup({
 	'morhetz/gruvbox',
 	{ 'nvim-lualine/lualine.nvim', dependencies = { 'nvim-tree/nvim-web-devicons' } },
 	'sheerun/vim-polyglot',
+	'williamboman/mason.nvim',
+	'williamboman/mason-lspconfig.nvim',
+	'neovim/nvim-lspconfig',
+	'mfussenegger/nvim-lint',
+	'mhartington/formatter.nvim',
+	{ 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
 })
 
 
@@ -60,3 +66,43 @@ require('lualine').setup {
 		lualine_z = { 'tabs' },
 	},
 }
+
+-- diagnostic keybindings
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+
+-- setup lsp
+require('mason').setup()
+require('mason-lspconfig').setup({
+	ensure_installed = { 'lua_ls', 'hls' }
+})
+
+local on_attach = function(_, _)
+	vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+	vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
+	vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+end
+
+local lspconfig = require('lspconfig')
+lspconfig.lua_ls.setup {
+	on_attach = on_attach,
+	settings = {
+		Lua = { diagnostics = { globals = { 'vim' } } }
+	}
+}
+lspconfig.hls.setup {
+	on_attach = on_attach
+}
+
+-- setup treesitter
+require('nvim-treesitter.configs').setup({
+  ensure_installed = { 'haskell', 'javascript', 'c', 'lua', 'vim', 'vimdoc', 'query' },
+  sync_install = false,
+  auto_install = true,
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+})
