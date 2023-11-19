0a5d70cae469576584a593e1597838ae7fb53f64
Florian Raith <florianraith00@gmail.com>
2023-11-19 02:33:17 +0100

    Format code

--- init.lua
+++ init.lua
@@ -139,11 +139,11 @@ require("mason-lspconfig").setup({
 })
 
 -- setup telescope
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 
 -- setup autocompletion
 local cmp = require("cmp")
