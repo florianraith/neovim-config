11fb048d4e81a4e7011dbafcded029d7af218653
Florian Raith <florianraith00@gmail.com>
2024-04-28 03:53:17 +0200

    Configure telescope

--- init.lua
+++ init.lua
@@ -159,6 +159,31 @@ vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 
+require("telescope").setup({
+  defaults = {
+    vimgrep_arguments = {
+      "rg",
+      "-L",
+      "--color=never",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
+      "--smart-case",
+    },
+    file_ignore_patterns = {
+      "node_modules",
+      "vendor",
+    },
+  },
+
+  pickers = {
+    find_files = {
+      hidden = true,
+    },
+  },
+})
+
 -- setup autocompletion
 local cmp = require("cmp")
 
