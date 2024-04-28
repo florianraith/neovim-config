354ee47a4b3659aa68ca279f7540e40f385d7834
Florian Raith <florianraith00@gmail.com>
2024-04-28 06:25:53 +0200

    Format code

--- init.lua
+++ init.lua
@@ -46,9 +46,9 @@ vim.keymap.set("n", "<leader>rp", function()
 end)
 
 -- Highlight when yanking (copying) text
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
   callback = function()
     vim.highlight.on_yank()
   end,
