1fde20d0261105b3966f1acdcac8d553bca3cf97
Florian Raith <florianraith00@gmail.com>
2024-09-17 23:34:01 +0200

    Add blade treesitter support

--- init.lua
+++ init.lua
@@ -431,6 +431,16 @@ require('nvim-treesitter.configs').setup {
     enable = true,
   },
 }
+
+local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
+parser_config.blade = {
+  install_info = {
+    url = 'https://github.com/EmranMR/tree-sitter-blade',
+    files = { 'src/parser.c' },
+    branch = 'main',
+  },
+  filetype = 'blade',
+}
 -- }}}
 
 -- Setup copilot {{{
